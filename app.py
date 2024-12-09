26014a2b9934798e007a29cc496fe1390c695b3f
yang <support@7trees.cn>
2024-12-09 17:45:06 +0800

    性能优化

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
-from flask import Flask, render_template, request, session, redirect, Response
-from flask_caching import Cache
+from flask import Flask, render_template, request, session, redirect
+from flask_socketio import SocketIO, emit
+from collections import deque
 import datetime
-import time
 
 app = Flask(__name__)
 app.secret_key = 'hard_key'
-cache = Cache(app, config={'CACHE_TYPE': 'simple', 'CACHE_DEFAULT_TIMEOUT': 60})
-messages_cache = []
-clients = []
+socketio = SocketIO(app)
+
+messages_cache = deque(maxlen=100)  # 使用 deque 限制缓存大小为100条
 
 @app.route('/')
 def index():
@@ -33,39 +33,17 @@ def logout():
         add_message(f'用户{user}退出了房间')
     return redirect('/login')
 
-@app.route('/send', methods=['POST'])
-def post_message():
-    message = request.form['message']
-    user = session.get('user', 'anonymous')
-    now = datetime.datetime.now().replace(microsecond=0).time()
-    add_message(f'[{now.isoformat()}] {user}: {message}')
-    return Response(status=204)
-
 def add_message(message):
     messages_cache.append(message)
-    # Limit cache size
-    if len(messages_cache) > 100:  # Keep the most recent 100 messages
-        messages_cache.pop(0)
+    # 通知所有连接的客户端新的消息
+    socketio.emit('new_message', message)
 
-    # Notify all clients about new message
-    for client in clients:
-        client.append(message)
-
-@app.route('/stream')
-def stream():
-    def generate():
-        client_message_cache = []
-        while True:
-            if messages_cache:
-                # Only send new messages to this client
-                new_messages = messages_cache[len(client_message_cache):]
-                if new_messages:
-                    for msg in new_messages:
-                        yield f"data: {msg}\n\n"
-                    client_message_cache.extend(new_messages)
-            time.sleep(1)
-
-    return Response(generate(), mimetype='text/event-stream')
+@socketio.on('send_message')
+def handle_send_message(message):
+    user = session.get('user', 'anonymous')
+    now = datetime.datetime.now().replace(microsecond=0).time()
+    formatted_message = f'[{now.isoformat()}] {user}: {message}'
+    add_message(formatted_message)
 
 if __name__ == '__main__':
-    app.run(debug=True, threaded=True)
+    socketio.run(app, debug=True, allow_unsafe_werkzeug=True)
